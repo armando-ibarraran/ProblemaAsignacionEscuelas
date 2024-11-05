b78934c3fe6147b4d3b980fa27999cb1328c4815
Carlos Armando Ibarrarán <46429550+armando-ibarraran@users.noreply.github.com>
2024-11-05 08:45:11 -0600

    costosBreakdown

--- incisoG.py
+++ incisoG.py
@@ -71,12 +71,12 @@ for i in range(cardI):
     for j in range(cardJ):
         for k in range(cardK):
             for s in range(3):
-                numSinis[s] += probs[j,k,s]*sols[s].x[indice(i,j,k)] if costosCam[s][j,k]==0 else 0
+                numSinis[s] += probs[j,k,s]*sols[s].x[indice(i,j,k)] 
 # Calcula los costos de esos siniestros
 costoSinis = [c*numSini for numSini in numSinis]
 # Imprime informacion
 print()
-print("Costos esperados por siniestros")
+print("Costos esperados por siniestros con soluciones previas")
 print("                                          c)          e)           f)")
 print("-------------------------------------------------------------------------")
 print(f"Numero esperado de siniestros:     {numSinis[0]:>10.2f}   {numSinis[1]:>10.2f}   {numSinis[2]:>10.2f}")
@@ -91,23 +91,42 @@ print()
 # Ahora, supongamos que nos interesa minimizar costos, tomando en cuenta los camiones y los siniestros
 # Para esto, a la antigua funcion objetivo, le sumamos el costo esperado de siniestros
 
-# Definimos los costos de la nueva funcion objetivo
+# Obtenemos la solución óptima para la nueva funcion objetivo
+# y de la original
 costosTot = [0,0,0]
-costosTotSinis = [0,0,0]
+costosTotNuevos = [0,0,0]
+solsNuevas = [None, None, None]
 for s in range(3):
-    costos = [0]*dimVars1D
+    costosNuevos = [0]*dimVars1D
     for i in range(cardI): 
         for j in range(cardJ):
             for k in range(cardK):
-                costos[indice(i,j,k)] = costosCam[s][j,k]
-                costos[indice(i,j,k)] += probs[j,k,s]*c if costosCam[s][j,k]==0 else 0
-    costosTot[s] =incisosABC.sol(costosCam[s], False).fun
-    costosTotSinis[s] = incisosABC.sol(costosCam[s], False, costos).fun
+                costosNuevos[indice(i,j,k)] = costosCam[s][j,k]
+                costosNuevos[indice(i,j,k)] += probs[j,k,s]*c 
+    costosTot[s] = incisosABC.sol(costosCam[s], False).fun
+    solsNuevas[s] = incisosABC.sol(costosCam[s], False, costosNuevos)
+    costosTotNuevos[s] = solsNuevas[s].fun
+
+
+# Calculamos, a partir de la solución, los costos nuevos propios de camiones y de siniestros
+costosCamNuev = [0,0,0]
+costosSinisNuev = [0,0,0]
+for s in range(3):
+    for i in range(cardI): 
+        for j in range(cardJ):
+            for k in range(cardK):
+                costosSinisNuev[s] += solsNuevas[s].x[indice(i,j,k)]*probs[j,k,s]*c 
+    costosCamNuev[s] = costosTotNuevos[s]-costosSinisNuev[s]
+
+                
 # Imprimimos la info
 print()
-print('Minimizando costos tomando en cuenta el costo de los siniestros')
-print("                                          c)          e)           f)")
+print('Minimizando costos con siniestros ponderados')
+print("                                                 c)           e)           f)")
 print("-------------------------------------------------------------------------")
-print(f"Costos totales sin siniestros:     ${costosTot[0]:>10.2f}   ${costosTot[1]:>10.2f}   ${costosTot[2]:>10.2f}")
-print(f"Costos totales con siniestros:     ${costosTotSinis[0]:>10.2f}   ${costosTotSinis[1]:>10.2f}   ${costosTotSinis[2]:>10.2f}")
-print()
+print(f"Costos totales originales (solo camiones ponderados):          ${costosTot[0]:>10,.2f}   ${costosTot[1]:>10,.2f}   ${costosTot[2]:>10,.2f}")
+print(f"Costos totales nuevos (con camiones y siniestros ponderados):  ${costosTotNuevos[0]:>10,.2f}   ${costosTotNuevos[1]:>10,.2f}   ${costosTotNuevos[2]:>10,.2f}")
+print(f"    Costos nuevos de camiones:                                 ${costosCamNuev[0]:>10,.2f}   ${costosCamNuev[1]:>10,.2f}   ${costosCamNuev[2]:>10,.2f}")
+print(f"    Costos nuevos de siniestros:                               ${costosSinisNuev[0]:>10,.2f}   ${costosSinisNuev[1]:>10,.2f}   ${costosSinisNuev[2]:>10,.2f}")
+
+
